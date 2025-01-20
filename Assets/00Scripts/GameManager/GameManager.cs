0fa43295d75cbf72254866fe33ab901499a4f69e
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2025-01-20 09:08:25 +0800

    added NextScene function in GameManager

--- Assets/00Scripts/GameManager/GameManager.cs
+++ Assets/00Scripts/GameManager/GameManager.cs
@@ -2,19 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
-	[SerializeField] Text player1PointsText;
-	[SerializeField] Text player2PointsText;
+	[SerializeField] Text player1PointsText, player2PointsText, player1ChatBox, player2ChatBox;
+	float player1Points, player2Points = 0;
+	int stage = 0;
 
-	[SerializeField] Text player1ChatBox;
-	[SerializeField] Text player2ChatBox;
-
-	float player1Points = 0;
-	float player2Points = 0;
-
+	private void Awake()
+	{
+		stage = PlayerPrefs.GetInt("stage", 0);
+		PlayerPrefs.DeleteAll();
+	}
 
 	void Start()
 	{
@@ -23,24 +24,42 @@ public class GameManager : MonoBehaviour
 		player1PointsText.text = player1Points.ToString();
 		player2PointsText.text = player2Points.ToString();
 
-		int randomNumber = Random.Range(0, 4);
-		string[] messages = new string[] { "ggez", "mao rato?", "boring", "uninstall", "banga" };
+		int randomNumber = Random.Range(0, 5);
+		string[] messages = new string[] { "ggez", "mao rato?", "boring", "uninstall", "banga", "HAHAHAHA" };
 
-		if(PlayerPrefs.GetInt("PlayerWon") == 1)
+		if (PlayerPrefs.GetInt("PlayerWon") == 1)
 		{
+			player2ChatBox.text = "";
 			player1ChatBox.text = messages[randomNumber];
 		}
-		else if(PlayerPrefs.GetInt("PlayerWon") == 2)
+		else if (PlayerPrefs.GetInt("PlayerWon") == 2)
 		{
+			player1ChatBox.text = "";
 			player2ChatBox.text = messages[randomNumber];
 		}
 		// PlayerPrefs.DeleteAll();
 	}
 
-
-	// Update is called once per frame
-	void Update()
+	public void NextScene()
 	{
-
+		switch (stage)
+		{
+			case 0:
+				SceneManager.LoadScene("Library");
+				break;
+			case 1:
+				SceneManager.LoadScene("Bridge");
+				break;
+			case 2:
+				SceneManager.LoadScene("PHRooms");
+				break;
+			case 3:
+				SceneManager.LoadScene("Finance");
+				break;
+			default:
+				break;
+		}
+		stage++;
+		PlayerPrefs.SetInt("stage", stage);
 	}
 }
