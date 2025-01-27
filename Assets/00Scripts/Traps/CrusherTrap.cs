74eedc8a6ef5dc7966fd5c4f824c418e5ff64c95
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2025-01-27 09:55:58 +0800

    added crusher trap

---
+++ Assets/00Scripts/Traps/CrusherTrap.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class CrusherTrap : MonoBehaviour
+{
+    float trapX, trapY;
+    float trapSpeed = 250;
+    Rigidbody2D rb;
+
+    bool fall = false;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        trapX = transform.position.x;
+        trapY = transform.position.y;
+    }
+
+    void trapFall()
+    {
+        rb.velocity = new Vector2(0, -10f * trapSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player1" || other.gameObject.tag == "Player2")
+        {
+            fall = true;
+        }
+    }
+
+    void Update()
+    {
+        if (fall)
+        {
+            trapFall();
+        }
+
+        if (transform.position.y < -40)
+        {
+            transform.position = new Vector2(trapX, trapY);
+        }
+    }
+}
